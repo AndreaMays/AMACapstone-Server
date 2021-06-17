1e17c86cb34f2102209c4f504286ebba691e2343
Andrea Mays <theallegromusicacademy70@gmail.com>
2021-06-17 15:34:18 -0500

    added StudentUser to admin.py for django admin

--- amaapi/admin.py
+++ amaapi/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from amaapi.models import LessonNotes
+from amaapi.models import LessonNotes, StudentUser
 
 admin.site.register(LessonNotes)
+admin.site.register(StudentUser)
