445893e1122990b6c3592421881ce6e79886a223
Andrea Mays <theallegromusicacademy70@gmail.com>
2021-06-23 14:01:04 -0500

    added amdin.py temp

--- amaapi/admin.py
+++ amaapi/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from amaapi.models import LessonNotes, StudentUser, Competitions
+from amaapi.models import LessonNotes, StudentUser, Competitions, Awards
 
 admin.site.register(LessonNotes)
 admin.site.register(StudentUser)
 admin.site.register(Competitions)
+admin.site.register(Awards)
