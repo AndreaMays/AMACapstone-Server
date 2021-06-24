b93d8da785d272fdf3d43e27388b8ddb819630e3
Andrea Mays <theallegromusicacademy70@gmail.com>
2021-06-23 21:53:43 -0500

    Added frontend url to setttings.py

--- ama/settings.py
+++ ama/settings.py
@@ -87,7 +87,8 @@ REST_FRAMEWORK = {
 }
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
-    'http://127.0.0.1:3000'
+    'http://127.0.0.1:3000',
+    'https://ecstatic-nobel-15b7d1.netlify.app'
 )
 
 WSGI_APPLICATION = 'ama.wsgi.application'
